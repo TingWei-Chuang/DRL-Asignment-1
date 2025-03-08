fd2fa16a5621ae54fdd1b7cd81c2975e21c4dd42
walker <b11902065@csie.ntu.edu.tw>
2025-03-08 23:48:07 +0800

    jjj

--- student_agent.py
+++ student_agent.py
@@ -4,9 +4,14 @@ import pickle
 import random
 import gym
 
-with open("q_table_18000.pkl", "rb") as file:
-    q_table = pickle.load(file)
-###
+with open("q_table_2_20000_1.pkl", "rb") as file:
+    q_table_1 = pickle.load(file)
+
+with open("q_table_2_20000_2.pkl", "rb") as file:
+    q_table_2 = pickle.load(file)
+
+q_table = q_table_1 | q_table_2
+
 def get_state(obs):
     taxi_row, taxi_col, station_0_row, station_0_col, station_1_row, station_1_col, station_2_row, station_2_col, station_3_row, station_3_col, obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look  = \
         obs[0], obs[1], obs[2], obs[3], obs[4], obs[5], obs[6], obs[7], obs[8], obs[9], obs[10], obs[11], obs[12], obs[13], obs[14], obs[15]
@@ -17,6 +22,8 @@ def get_state(obs):
         elif a > b:
             return 1
         return 0
+    def manhattan(x1, y1, x2, y2):
+        return abs(x1 - x2) + abs(y1 - y2)
     state += [
         dir(taxi_row, station_0_row),
         dir(taxi_col, station_0_col),
@@ -28,6 +35,12 @@ def get_state(obs):
         dir(taxi_col, station_3_col)
     ]
     state += [
+        manhattan(taxi_row, taxi_col, station_0_row, station_0_col),
+        manhattan(taxi_row, taxi_col, station_1_row, station_1_col),
+        manhattan(taxi_row, taxi_col, station_2_row, station_2_col),
+        manhattan(taxi_row, taxi_col, station_3_row, station_3_col),
+    ]
+    state += [
         obstacle_north,
         obstacle_south,
         obstacle_east,
@@ -37,12 +50,12 @@ def get_state(obs):
         passenger_look,
         destination_look
     ]
-    
+
     pickup = False
     if not passenger_look:
         pickup = False
     else:
-        pickup = np.random.choice([True, False])
+        pickup = np.random.choice([True, False], p=[0.9, 0.1])
     state += [
         pickup
     ]
