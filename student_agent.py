8833ef293eea39d258201dfb1cd3d01693f53959
walker <b11902065@csie.ntu.edu.tw>
2025-03-13 01:03:07 +0800

    fjsi

--- student_agent.py
+++ student_agent.py
@@ -4,7 +4,7 @@ import pickle
 import random
 import gym
 
-with open("qq.pkl", "rb") as file:
+with open("new_qq.pkl", "rb") as file:
     q_table = pickle.load(file)
 steps = 0
 pickup = False
@@ -80,12 +80,14 @@ def get_action(obs):
     if state not in q_table:
         action = np.random.choice(6) # Choose a random action
     else:
-        #p = softmax(q_table[state])
-        #action = np.random.choice(6, p=p)        
+        n_act = 4
+        if (state[12] or state[13]) and ((state[0] == 0 and state[1] == 0) or (state[2] == 0 and state[3] == 0) \
+            or (state[4] == 0 and state[5] == 0) or (state[6] == 0 and state[7] == 0)):
+            n_act = 6
         if np.random.rand() < 0.05:
-            action = np.random.choice(6)
+            action = np.random.choice(n_act)
         else:
-            action = np.argmax(q_table[state])
+            action = np.argmax(q_table[state][:n_act])
     if not pickup:
         at_pickup = state[12] and ((state[0] == 0 and state[1] == 0) or (state[2] == 0 and state[3] == 0) \
             or (state[4] == 0 and state[5] == 0) or (state[6] == 0 and state[7] == 0))
