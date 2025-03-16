8fdafd8c55f42514af98273779086b82965d9ca2
walker <b11902065@csie.ntu.edu.tw>
2025-03-16 19:59:12 +0800

    jfdis

--- student_agent.py
+++ student_agent.py
@@ -103,7 +103,7 @@ def get_action(obs):
         action_pickup = action == 4
         pickup = at_pickup == True and action_pickup
         if not_at_pickup:
-            print(visitsA)
+            print(visitsA, flush=True)
             visitsA += 1
             visitsA = min(visitsA, 3)
     else:
@@ -113,7 +113,7 @@ def get_action(obs):
         action_drop = action == 5
         pickup = not (at_dest and action_drop)
         if not_at_dest:
-            print(visitsB)
+            print(visitsB, flush=True)
             visitsB += 1
             visitsB = min(visitsB, 3)
         if not pickup:
